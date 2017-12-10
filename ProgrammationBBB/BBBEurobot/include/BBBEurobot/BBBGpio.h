d14ba2316ead64bb1791d6413c8e0bc33c29897a
Matthieu Vigne <matthieu.vigne@laposte.net>
2017-12-10 17:55:11 +0100

    [BBBEurobot] Add gpio handling code to BBBEurobot library.

--- ProgrammationBBB/BBBEurobot/include/BBBEurobot/BBBGpio.h
+++ ProgrammationBBB/BBBEurobot/include/BBBEurobot/BBBGpio.h
@@ -9,14 +9,51 @@
 
 	#include <stdint.h>
 	#include <glib.h>
-	
-	
-	/// \brief Enable the Beaglebone serial ports.
+
+
+	/// \brief Enable the Beaglebone serial ports and gpio.
 	/// \details This function must be called at the start of the program, before any access to a
-	///          serial port. This function returns nothing, but stops the program execution is uncessussful.
+	///          serial port or gpio. This function returns nothing, but stops the program execution is uncessussful.
 	///
-	/// \warning This function uses the device tree overlay file "Eurobot-00A0.dtbo". This file must exist in 
+	/// \warning This function uses the device tree overlay file "Eurobot-00A0.dtbo". This file must exist in
 	///          /lib/firmware, else this function fails. This function only activates this overlay : the behavior
 	///          of the system entirely depends on this file being properly written.
-	void gpio_enableSerialPorts();
+	void gpio_enablePorts();
+
+
+	/// \brief Export a gpio pin, setting the direction.
+	/// \details Exporting a gpio pin maks it available in user-space. In practice, this create a folder in
+	///			 /sys/class/gpio/gpioX, where X is the gpio number. However, this does not necessarily give control
+	///			 of the pin : this is done at pinmux level in the overlay (see Eurobot overlay). As such, if the pin
+	///			 is used for something else (serial communication for example), an operation done on the gpio is meaningless.
+	///			 Note also that, while exporting the gpio does set its direction, there is no way in user space to
+	///			 change the pullups on the pin.
+	///			 This function never fails, but one again, it is up to the user to check that the pin is configured correctly.
+	///
+	/// \param[in] pin gpio pin number (kernel representation).
+	/// \param[in] direction gpio direction, either "in" or "out"
+	///
+	/// \return 0 on success, -1 on file access error.
+	int gpio_exportPin(int pin, gchar *direction);
+
+	/// \brief Read a digital input.
+	/// \details The pin must be configured as an input for the return value to make sense.
+	///
+	/// \param[in] pin gpio pin number (kernel representation).
+	///
+	/// \return Positive value on success : 0 for low, 1 for high.
+	///			Negative value on failure: -1: reading error (probably pin not enabled). -2: pin enabled but not an input.
+	int gpio_digitalRead(int pin);
+
+
+	/// \brief Write to a digital output.
+	/// \details The pin must be configured as an input for the return value to make sense.
+	///
+	/// \param[in] pin gpio pin number (kernel representation).
+	/// \param[in] value value to write: 0 for low, else high
+	///
+	/// \return 0 on success
+	///			Negative value on failure: -1: reading error (probably pin not enabled). -2: pin enabled but not an output.
+	int gpio_digitalWrite(int pin, int value);
+
 #endif
--- ProgrammationBBB/BBBEurobot/src/BBBGpio.c
+++ ProgrammationBBB/BBBEurobot/src/BBBGpio.c
@@ -13,10 +13,10 @@ gboolean isEurobotEnabled()
 		printf("Enabling serial ports failed: cannot open cape manager (/sys/devices/bone_capemgr.9/slots).\n");
 		exit(0);
 	}
-	
+
 	gboolean isEurobotEnabled = FALSE;
-	gchar *line="";
-	
+	gchar *line= NULL;
+
 	// Check if the overlay is already enabled
 	while ( g_io_channel_read_line (slot, &line, NULL, NULL, NULL )!= G_IO_STATUS_EOF)
 	{
@@ -29,25 +29,27 @@ gboolean isEurobotEnabled()
 	}
 	g_free(line);
 	g_io_channel_shutdown(slot, TRUE, NULL);
+	g_io_channel_unref(slot);
 	return isEurobotEnabled;
 }
 
-void gpio_enableSerialPorts()
+void gpio_enablePorts()
 {
 	// Check if the overlay is already enabled.
 	if(isEurobotEnabled())
 		return;
+
 	// Else, let us first check that the overlay file exists.
-	gchar *overlayFile = "/lib/firmware/Eurobot-00A0.dtbo";
+	gchar *overlayFile = g_strdup_printf("/lib/firmware/Eurobot-00A0.dtbo");
 	if(g_file_test(overlayFile, G_FILE_TEST_EXISTS) == FALSE)
 	{
 		printf("Enabling serial ports failed: cannot find overlay (%s).\n", overlayFile);
 		exit(0);
 	}
-	
+	g_free(overlayFile);
 	// Enable the overlay.
 	system("echo Eurobot > /sys/devices/bone_capemgr.9/slots");
-	
+
 	// Check that the overlay is indeed enabled.
 	if(isEurobotEnabled())
 		return;
@@ -56,4 +58,144 @@ void gpio_enableSerialPorts()
 		printf("Enabling serial ports failed: unknown error.\n");
 		exit(0);
 	}
+}
+
+// Internal function : get the content of the first line of the file. Returns TRUE on success, FALSE on error.
+gboolean getFileContent(gchar *fileName, gchar **output)
+{
+	GError *error = NULL;
+	GIOChannel *slot = g_io_channel_new_file (fileName, "r", &error);
+	if(error != NULL)
+	{
+		g_error_free(error);
+		return FALSE;
+	}
+	g_io_channel_read_line (slot, output, NULL, NULL, &error);
+	g_io_channel_shutdown(slot, TRUE, NULL);
+	g_io_channel_unref(slot);
+	if(error != NULL)
+	{
+		g_error_free(error);
+		return FALSE;
+	}
+	return TRUE;
+}
+
+int gpio_digitalRead(int pin)
+{
+	// Get path to gpio direction file.
+	gchar *directionFile = g_strdup_printf("/sys/class/gpio/gpio%d/direction", pin);
+	gchar *direction = "";
+	gboolean returnCode = getFileContent(directionFile, &direction);
+	g_free(directionFile);
+
+	if(returnCode == FALSE)
+		return -1;
+	// Check file is an input.
+	if(g_strcmp0 (direction, "in\n") != 0)
+	{
+		g_free(direction);
+		return -2;
+	}
+	g_free(direction);
+
+	// Read gpio value.
+	gchar *valueFile = g_strdup_printf("/sys/class/gpio/gpio%d/value", pin);
+	gchar *value = NULL;
+	returnCode = getFileContent(valueFile, &value);
+	g_free(valueFile);
+
+	if(returnCode == FALSE)
+	{
+		g_free(value);
+		return -1;
+	}
+	int pinValue = g_ascii_strtoll(value, NULL, 10);
+	g_free(value);
+	return pinValue;
+}
+
+int gpio_digitalWrite(int pin, int value)
+{
+	// Get path to gpio direction file.
+	gchar *directionFile = g_strdup_printf("/sys/class/gpio/gpio%d/direction", pin);
+	gchar *direction = "";
+	gboolean returnCode = getFileContent(directionFile, &direction);
+	g_free(directionFile);
+
+	if(returnCode == FALSE)
+		return -1;
+	// Check file is an input.
+	if(g_strcmp0 (direction, "out\n") != 0)
+	{
+		g_free(direction);
+		return -2;
+	}
+	g_free(direction);
+
+	// Write gpio value.
+	gchar *valueFile = g_strdup_printf("/sys/class/gpio/gpio%d/value", pin);
+	GError *error = NULL;
+	GIOChannel *slot = g_io_channel_new_file (valueFile, "w", &error);
+	g_free(valueFile);
+	if(error != NULL)
+	{
+		g_error_free(error);
+		return -1;
+	}
+	gchar *valueString = g_strdup_printf("%d\n", (value == 0 ? 0 : 1));
+	g_io_channel_write_chars(slot, valueString, -1, NULL, &error);
+	g_io_channel_shutdown(slot, TRUE, NULL);
+	g_free(valueString);
+	g_io_channel_unref(slot);
+	if(error != NULL)
+	{
+		g_error_free(error);
+		return -1;
+	}
+	return 0;
+}
+
+
+int gpio_exportPin(int pin, gchar *direction)
+{
+	// Export pin value.
+	gchar *exportFile = g_strdup_printf("/sys/class/gpio/export");
+	GError *error = NULL;
+	GIOChannel *slot = g_io_channel_new_file (exportFile, "a", &error);
+	g_free(exportFile);
+	if(error != NULL)
+	{
+		g_error_free(error);
+		return -1;
+	}
+	gchar *valueString = g_strdup_printf("%d\n", pin);
+	g_io_channel_write_chars(slot, valueString, -1, NULL, &error);
+	g_io_channel_shutdown(slot, TRUE, NULL);
+	g_free(valueString);
+	g_io_channel_unref(slot);
+	if(error != NULL)
+	{
+		g_error_free(error);
+		return -1;
+	}
+
+	// Set gpio direction.
+	gchar *directionFile = g_strdup_printf("/sys/class/gpio/gpio%d/direction", pin);
+	slot = g_io_channel_new_file (directionFile, "w", &error);
+	g_free(directionFile);
+	if(error != NULL)
+	{
+		g_error_free(error);
+		return -1;
+	}
+	g_io_channel_write_chars(slot, direction, -1, NULL, &error);
+	g_io_channel_shutdown(slot, TRUE, NULL);
+	g_io_channel_unref(slot);
+	if(error != NULL)
+	{
+		g_error_free(error);
+		return -1;
+	}
+	return 0;
 }
